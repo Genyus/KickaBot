791ccc9a35d3da8111929bb27f316606e3b827ee
Genyus <genyus@gmail.com>
2016-02-15 13:29:24 +0000

    refactor(server): Improve bunyan logging output

--- server.js
+++ server.js
@@ -90,16 +90,16 @@ var logger = require('./lib/logger');
     }
 
     function query_bot (bot){
-        logger.debug(util.inspect(bot, { showHidden: true, depth: 1 }));
+        logger.debug({'bot': util.inspect(bot, { showHidden: true, depth: 1 })});
         var promise = bot.getMe();//throw_error();
         var timeout = 60000;
 
-        logger.debug(util.inspect(promise, { showHidden: false, depth: 2 }));
+        logger.debug({'promise': util.inspect(promise, { showHidden: false, depth: 2 })});
         promise.then(function (data) {
-            logger.debug(util.inspect(promise, { showHidden: false, depth: 2 }));
+            logger.debug({'promise': util.inspect(promise, { showHidden: false, depth: 2 })});
             setTimeout (function() { query_bot(bot); }, timeout); //queue for next ping in the next predefined interval
         }, function (err) {
-            logger.debug(util.inspect(promise, { showHidden: false, depth: 2 }));
+            logger.debug({'promise': util.inspect(promise, { showHidden: false, depth: 2 })});
             logger.warn(err);
             bot = start_bot();
             setTimeout (function() { query_bot(bot); }, timeout); //queue for next ping in the next predefined interval
